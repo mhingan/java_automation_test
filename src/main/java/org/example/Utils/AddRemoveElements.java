eda8170196352caf0ba52454239e52637c8497e3
Mihaita <124569822+mhingan@users.noreply.github.com>
2024-07-28 10:23:45 +0300

    test 1 - version 1

--- src/main/java/org/example/Utils/AddRemoveElements.java
+++ src/main/java/org/example/Utils/AddRemoveElements.java
@@ -1,2 +1,36 @@
-package org.example.Utils;public class AddRemoveElements {
+package org.example.Utils;
+
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class AddRemoveElements {
+
+    String link_test_1 = "https://the-internet.herokuapp.com/add_remove_elements/";
+
+    @FindBy(xpath = "/html/body/div[2]/div/div/button")
+    private WebElement add_button;
+
+    @FindBy(xpath = "/html/body/div[2]/div/div/div/button")
+    private WebElement delete_buttons;
+
+    @FindBy(xpath = "//*[@id=\"content\"]/h3")
+    private WebElement page_title;
+
+
+    public String getLink(){
+        return link_test_1;
+    }
+
+    public WebElement getAddBttn(){
+        return add_button;
+    }
+
+    public WebElement getDeleteBttn(){
+        return delete_buttons;
+    }
+
+    public WebElement getPage_title(){
+        return page_title;
+    }
 }
